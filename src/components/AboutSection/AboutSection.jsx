a4cd804b61948ffceb8c4c292190674d2bf78cad
HelixW <shreyas.2000@hotmail.com>
2021-07-13 18:59:34 +0530

    [8] Animation added to about

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -1,13 +1,48 @@
+import { useEffect } from "react";
+import gsap from "gsap";
+
+// Stylesheets
 import "./AboutSection.css";
 
 const AboutSection = () => {
+  useEffect(() => {
+    const tl = gsap.timeline();
+
+    tl.to("#about-heading", {
+      scrollTrigger: "#about-heading",
+      opacity: 1,
+      duration: 1.5,
+      delay: 2,
+    })
+      .from("#about-heading", {
+        scrollTrigger: "#about-heading",
+        x: -100,
+        duration: 1.5,
+        delay: -1.5,
+      })
+      .to("#about-content", {
+        scrollTrigger: "#about-content",
+        opacity: 1,
+        duration: 1.5,
+        delay: -1,
+      })
+      .from("#about-content", {
+        scrollTrigger: "#about-content",
+        x: -100,
+        duration: 1.5,
+        delay: -1.5,
+      });
+  }, []);
+
   return (
     <section className="about" id="about">
       <header>
-        <h2 className="heading">About Me</h2>
+        <h2 className="heading" id="about-heading">
+          About Me
+        </h2>
       </header>
       <main>
-        <p className="text">
+        <p className="text" id="about-content">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur
           quo ea, modi laudantium quisquam dolorem deserunt adipisci! Neque amet
           recusandae optio, iste ipsum reiciendis iure sunt fugit ducimus quos,
